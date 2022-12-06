08e1ac5393fe486d2859bf8a34364e53c0e88dfa
yaangha <y210he@gmail.com>
2022-12-06 20:01:10 +0900

    백업용 커밋

--- src/main/java/site/book/project/repository/BookRepository.java
+++ src/main/java/site/book/project/repository/BookRepository.java
@@ -1,13 +1,14 @@
 package site.book.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import site.book.project.domain.Book;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
-
-	
-	
-	
-	
+    
+    // (하은) 작가의 다른 책 정보 찾기
+    List<Book> findAllByAuthor(String author);
+    
 }
