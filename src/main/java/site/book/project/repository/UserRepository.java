023327136bac374855c24dece287fd8631797bfa
gitPrintln <ghdcks101@naver.com>
2022-12-06 14:45:31 +0900

    PostReply

--- src/main/java/site/book/project/repository/UserRepository.java
+++ src/main/java/site/book/project/repository/UserRepository.java
@@ -8,7 +8,8 @@ import site.book.project.domain.User;
 
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    
+
+
     // 아이디로 유저 정보 찾기
     @Query("select u from USERS u where u.username = :username")
     User findByName(@Param(value="username") String username);
