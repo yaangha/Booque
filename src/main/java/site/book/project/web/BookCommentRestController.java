3a6af09baf0e93bdeff2ba40ef2573de584f42da
choisilver <zmyn1004@gmail.com>
2022-12-06 17:34:01 +0900

    상세페이지의 한줄평(likes안함)

--- src/main/java/site/book/project/web/BookCommentRestController.java
+++ src/main/java/site/book/project/web/BookCommentRestController.java
@@ -1,14 +1,18 @@
 package site.book.project.web;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 // 책 한줄평 REST 컨트롤러( AJax)
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import site.book.project.dto.BookCommentReadDto;
 import site.book.project.dto.BookCommentRegisterDto;
 import site.book.project.service.BookCommentService;
 
@@ -17,15 +21,28 @@ import site.book.project.service.BookCommentService;
 @RestController  // response.data
 public class BookCommentRestController {
     
-    private final BookCommentService commentService;
+    private final BookCommentService bookCommentService;
     
-    @PostMapping("/api/comment")
+    @PostMapping("/api/comment") // 한줄평 insert
     public ResponseEntity<Integer> registerComment(@RequestBody BookCommentRegisterDto dto){
         log.info("한줄평 dto ={}",dto);
         
+        Integer commentId = bookCommentService.create(dto);
         
-        return null;
+        
+        return ResponseEntity.ok(commentId);
     }
+    
+    @GetMapping("/api/comment/all/{bookId}")
+    public ResponseEntity<List<BookCommentReadDto>> readAllComment(@PathVariable Integer bookId) {
+        log.info("comment여기가 바로! bookREST!!! bookId= {}", bookId);
+        
+        List<BookCommentReadDto> list = bookCommentService.readComment(bookId);
+        
+        return ResponseEntity.ok(list);
+        
+    }
+    
     
     
     
