f017c2ffb2a1a7504a4727da69f4b22bb3a96f2e
jinghk1212 <jinghk1212@naver.com>
2022-12-07 19:25:37 +0900

    User관련 작성

--- src/main/java/site/book/project/config/SecurityConfig.java
+++ src/main/java/site/book/project/config/SecurityConfig.java
@@ -1,21 +1,21 @@
 package site.book.project.config;
 
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-         // Spring Security는 GET 방식을 제외한 POST/PUT/DELETE 요청에서 
-         // CERF 토큰을 요구함.
-         // POST/PUT/DELETE 요청에서 CSRF 토큰을 서버로 전송하지 않으면 403(forbidden:권한 없음) 에러가 발생.
-         // 기능 구현을 간단히하기 위해서 Spring Security의 CFRS 기능을 비활성화. 
-        http.csrf().disable();
-        
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//         // Spring Security는 GET 방식을 제외한 POST/PUT/DELETE 요청에서 
+//         // CERF 토큰을 요구함.
+//         // POST/PUT/DELETE 요청에서 CSRF 토큰을 서버로 전송하지 않으면 403(forbidden:권한 없음) 에러가 발생.
+//         // 기능 구현을 간단히하기 위해서 Spring Security의 CFRS 기능을 비활성화. 
+//        http.csrf().disable();
+//        
+//        return http.build();
+//    }
 }
