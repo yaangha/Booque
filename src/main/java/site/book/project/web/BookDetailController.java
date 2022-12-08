0536c659002cced8ac40e18707c6d63baf6f17af
yaangha <y210he@gmail.com>
2022-12-08 20:58:49 +0900

    order template 작업 중..

--- src/main/java/site/book/project/web/BookDetailController.java
+++ src/main/java/site/book/project/web/BookDetailController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -52,6 +53,13 @@ public class BookDetailController {
         model.addAttribute("postList", postList );      
         
         return "book/detail";
+    }
+    
+    // (하은) 장바구니 버튼 -> 계속 쇼핑하기
+    @PostMapping("/continueShopping")
+    public String continueShopping(Integer id) {
+        
+        return "redirect:/detail?id=" + id;
     }
     
 }
