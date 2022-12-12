d78c31f276d95345094679de556b503a488ee262
Jihye-Ryu <limegrey876@gmail.com>
2022-12-12 18:38:55 +0900

    jihye_detail_1212
    - 위시리스트 버튼, 한줄평 좋아요 버튼 효과 추가
    - 위시리스트 service, controller 클래스에 추가
    - 위시리스트 JS 파일 추가(작업 중)

--- src/main/java/site/book/project/service/BookWishService.java
+++ src/main/java/site/book/project/service/BookWishService.java
@@ -3,6 +3,7 @@ package site.book.project.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -38,5 +39,29 @@ public class BookWishService {
 
         return wishBookInfo;
     }
+    
+    
+    // (지혜) 위시리스트 담기/취소
+    public String clickWishButton(BookWish wish) {
+        log.info("clickWishButton(dto={})", wish);
+        
+        String result;
+        
+        try {
+            // 위시리스트 테이블에 추가 후 "selected" 결과값 리턴
+            bookWishRepository.save(wish);      
+            result = "selected";
+        } catch (DataAccessException e) {
+            // 위시리스트 테이블에서 삭제 후 "unselected" 결과값 리턴
+            bookWishRepository.delete(wish);
+            result = "unselected";
+        }
+        
+        return result;
+    }
+    
+    
+    
+    
     
 }
