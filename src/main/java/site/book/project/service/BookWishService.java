125ea8f8f4ad4a9574cf82c5354bd858785d59df
Jihye-Ryu <limegrey876@gmail.com>
2022-12-13 16:27:06 +0900

    jihye_wish_1213
    책 detail 페이지에서 위시리스트 기능 구현 성공!!!!!!!
    - 빈 하트 누르면 빨강하트로 바뀌고 wishList 테이블에 DB 추가됨
    - 빨강하트를 다시 누르면 테이블에서 해당 DB 삭제됨
    - (추후 수정 필요-로그인 기능과 연결) 빨강하트 된 상태에서 화면 새로고침하면 다시 빈 하트 되는 문제

--- src/main/java/site/book/project/service/BookWishService.java
+++ src/main/java/site/book/project/service/BookWishService.java
@@ -10,8 +10,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import site.book.project.domain.Book;
 import site.book.project.domain.BookWish;
+import site.book.project.domain.User;
 import site.book.project.dto.BookWishDto;
+import site.book.project.repository.BookRepository;
 import site.book.project.repository.BookWishRepository;
+import site.book.project.repository.UserRepository;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,6 +22,8 @@ import site.book.project.repository.BookWishRepository;
 public class BookWishService {
 
     private final BookWishRepository bookWishRepository;
+    private final BookRepository bookRepository;
+    private final UserRepository userRepository;
     
     // (하은) 사용자 찜한 리스트 찾기
     // 1) 각 사용자에 저장된 찜한 책 리스트 뽑기
@@ -41,23 +46,56 @@ public class BookWishService {
     }
     
     
-    // (지혜) 위시리스트 담기/취소
-    public String clickWishButton(BookWish wish) {
-        log.info("clickWishButton(dto={})", wish);
+    // (지혜) 위시리스트 담기/취소 및 위시 버튼(하트) 색상 상태 전환
+    public String changeWishButton(Integer userId, Integer bookId) {
+        log.info("changeWishButton(userId={}, bookId={})", userId, bookId);
+        
+        // DB에 일치하는 데이터가 있는지(이 유저가 이 책을 위시리스트에 담은 상태인지) 체크
+        BookWish checkWish = bookWishRepository.findByUserIdAndBookBookId(userId, bookId);
         
         String result;
         
-        try {
+        if (checkWish == null) {   // 위시리스트에 담지 않은 상태라면
+            
+            User user = userRepository.findById(userId).get();
+            Book book = bookRepository.findById(bookId).get();
+            BookWish wish = new BookWish(null, user, book);
+            
             // 위시리스트 테이블에 추가 후 "selected" 결과값 리턴
-            bookWishRepository.save(wish);      
-            result = "selected";
-        } catch (DataAccessException e) {
+            bookWishRepository.save(wish);
+            result = "selected";  // 하트를 꽉 찬(빨강) 하트로 바꾸기 위한 문자열
+            log.info("Add To WishList(wishListId={}, userId={}, bookId={}), result={})", 
+                    wish.getBookWishId(), wish.getUser().getId(), wish.getBook().getBookId(), result);
+        } else {   // 위시리스트에 이미 담긴 상태라면
+            
             // 위시리스트 테이블에서 삭제 후 "unselected" 결과값 리턴
-            bookWishRepository.delete(wish);
-            result = "unselected";
+            bookWishRepository.delete(checkWish);
+            result = "unselected";  // 하트를 빈 하트로 바꾸기 위한 문자열
+            log.info("Delete From WishList(wishListId={}, userId={}, bookId={}), result={})",
+                    checkWish.getBookWishId(), checkWish.getUser().getId(), checkWish.getBook().getBookId(), result);
+            
         }
         
         return result;
+        
+        
+//        try {
+//            // 위시리스트 테이블에 추가 후 "selected" 결과값 리턴
+//            bookWishRepository.save(wish);
+//            result = "selected";
+//            log.info("Add To WishList(wishListId={}, userId={}, bookId={}), result={})", 
+//                    wish.getBookWishId(), wish.getUser().getId(), wish.getBook().getBookId(), result);
+//        } catch (DataAccessException e) {  // 이미 중복값이 DB에 있어서 exception 발생하면
+//            // 위시리스트 테이블에서 삭제 후 "unselected" 결과값 리턴
+//            bookWishRepository.delete(wish);
+//            result = "unselected";
+//            log.info("Delete From WishList(wishListId={}, userId={}, bookId={}), result={})",
+//                    wish.getBookWishId(), wish.getUser().getId(), wish.getBook().getBookId(), result);
+//        }
+//        
+//        return result;
+        
+        
     }
     
     
--- src/main/resources/static/js/wishList.js
+++ src/main/resources/static/js/wishList.js
@@ -11,42 +11,48 @@
     function clickBtnWish () {
         
         // 현재 로그인되어 있는 유저 아이디 찾기
-        // let userId = document.querySelector('#userId').value;
+        const userId = document.querySelector('#userId').value;
         // 찜(위시리스트) 누른 책 id 찾기
-        // let bookId = document.querySelector('$id').value;
-        // 상태 반영할 찜(위시리스트) 아이콘(=버튼) 찾기
-        let btnWish = document.querySelector('#btnWish');
+        const bookId = document.querySelector('#id').value;
+        // 하트 버튼 옆의 텍스트 찾기
+        const wishText = document.querySelector('#wishText');
         
-        // if (userId == undefined) {
-            // alret("로그인 후 가능한 서비스입니다.");
+        console.log("clickBtnWish: userId=" + userId + ", bookId=" + bookId);
+        
+        if (userId == undefined) {
+            alret("로그인 후 가능한 서비스입니다.");
             // 로그인 모달 열기?
-            // return false;
-        // }
+            return false;
+        }
         
-        $.axios ( {
+        $.ajax ( {
             url: "/book/wishList",
             type: "Get",
             data: {
-                // userId: userId, 
-                // bookId:bookId
+                userId: userId, 
+                bookId:bookId
                 },
             dataType: "text",
             
             success: function(wishSelectedOrNot) {
-                let html = '';
-                html += '<span id="btnWish">';
+                let wishHtml = '';
+                let wishText2 = '';
                 
                 if (wishSelectedOrNot == "selected") {
-                    html += 
-                '<img id="wishFull" style="width:25px;" alt="" src=" /images/likeFull.png " />';
+                    wishHtml += 
+                '<img id="wishFull" style="width:25px; display:inline;" alt="취소" src=" /images/likeFull.png " />';
+                    wishText2 += '위시리스트에 담은 책입니다.';
+                
                 } else {
-                    html +=
-                '<img id="wishEmpty" style="width:25px;" alt="" src=" /images/likeEmpty.png " />';
+                    wishHtml +=
+                '<img id="wishEmpty" style="width:25px;" alt="추가" src=" /images/likeEmpty.png " />'
+                + '<img id="wishFull" style="width:25px;" alt="취소" src=" /images/likeFull.png " />';
+                    wishText2 += '위시리스트 담기';
+                
                 }
                 
-                html += '</span>';
-                
-                btnWish.innerHTML = html;
+                btnWish.innerHTML = wishHtml;
+                wishText.innerHTML = wishText2;
                 
             },
             
