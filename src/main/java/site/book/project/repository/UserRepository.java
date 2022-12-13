7cc3449c5644c70826d94570b5e5145440475e29
jinghk1212 <jinghk1212@naver.com>
2022-12-13 16:09:48 +0900

    Merge branch 'develop' of https://github.com/yaangha/Booque.git into hyeok1208

--- src/main/java/site/book/project/repository/UserRepository.java
+++ src/main/java/site/book/project/repository/UserRepository.java
@@ -1,18 +1,27 @@
 package site.book.project.repository;
 
+import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import site.book.project.domain.User;
+import site.book.project.dto.UserSigninDto;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     // 아이디로 유저 정보 찾기
     // @Query("select u from USERS u where u.username = :username")
     // User findByName(@Param(value="username") String username);
- 
+    @Transactional
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByUsername(String username);
+    Optional<User> findByNickName(String nickname);
+    Optional<User> findByEmail(String email);
+    
+    //List<User> searchByPw(@Param(value="userId") String userId, @Param(value = "password") String password);
     
 }
