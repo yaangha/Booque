cec28a4345ceb20584696739974095e0f05e0b0d
Jihye-Ryu <limegrey876@gmail.com>
2022-12-15 16:50:41 +0900

    jihye_1215

--- src/main/java/site/book/project/service/SortService.java
+++ src/main/java/site/book/project/service/SortService.java
@@ -7,56 +7,11 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import site.book.project.domain.Book;
-import site.book.project.repository.SearchRepository;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class SortService {
-
-    private final SearchRepository searchRepository;
     
-    /*
-    public List<Book> sort(String group, String category) {
-        List<Book> list;
-        
-        list = searchRepository.findByBookgroup(group);
-        
-        return list;
-    }
-    
-    
-    public List<Book> search(String type, String keyword) {
-        List<Book> list = null;
-
-        // 드랍 다운 검색 타입(value 속성):
-        if(type.equals("all")) { // 통합 검색
-            return list = searchRepository.unifiedSearchByKeyword(keyword);
-        }  else if (type.equals("do")) { // 국내 도서 검색
-            return list = searchRepository.domesticSearchByKeyword(keyword);
-        } else if (type.equals("fo")) { // 외국 도서 검색
-            return list = searchRepository.foreignSearchByKeyword(keyword);
-        } else if (type.equals("au")) { // 저자 검색
-            return list = searchRepository.authorSearchByKeyword(keyword);
-        }
-        return list;
-    }
-    
-    public List<Book> research(String type, String keyword, String order){
-        List<Book> list = null;
-        switch(order) {
-        case "highPrice": // 최고가순 정렬
-            list = searchRepository.researchOrderByHighPrice(keyword);
-            break;
-        case "lowPrice": // 최저가순 정렬
-            list = searchRepository.researchOrderByLowPrice(keyword);
-            break;
-        case "publishedDate": // 신상품순 정렬
-            list = searchRepository.researchOrderByPublishedDate(keyword); 
-            break;
-        }
-
-        return list;
-    }*/
     
 }
--- src/main/java/site/book/project/web/SortContoller.java
+++ src/main/java/site/book/project/web/SortContoller.java
@@ -10,54 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import site.book.project.domain.Book;
-import site.book.project.dto.SearchQueryDataDto;
-import site.book.project.service.PostService;
-import site.book.project.service.SearchService;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/search")
 @Slf4j
 public class SortContoller {
     
-    private final SearchService searchService;
 
-    /*
-    @GetMapping("")
-    public String search() {
-        log.info("MainSearch()");
-        return "/search";
-    }
     
-    // 검색 기능
-    @GetMapping("/s")
-    public String search(SearchQueryDataDto dto, Model model) {
-        log.info("request : search(type={}, keyword={})", dto.getType(), dto.getKeyword());
-        String type = dto.getType();
-        String keyword = dto.getKeyword();
-        
-        List<Book> searchList = searchService.search(dto.getType(), dto.getKeyword());
-        
-        
-        model.addAttribute("searchList", searchList);
-        model.addAttribute("storedType", type);
-        model.addAttribute("storedKeyword", keyword);
-        return "/search";
-    }
-    
-    // 검색 결과 재정렬(type, keyword를 가지고 다시 order by ?, ?부분만 원하는 order에 따라 바꿔서 재검색
-    @GetMapping("/ss")
-    public String research(SearchQueryDataDto dto, Model model) {
-        log.info("request : search(type={}, keyword={}, order={})", dto.getType(), dto.getKeyword(), dto.getOrder());
-        String type = dto.getType();
-        String keyword = dto.getKeyword();
-        
-        List<Book> searchList = searchService.research(dto.getType(), dto.getKeyword(), dto.getOrder());
-        
-
-        model.addAttribute("searchList", searchList);
-        model.addAttribute("storedType", type);
-        model.addAttribute("storedKeyword", keyword);
-        return "/search";
-    }*/
 }
