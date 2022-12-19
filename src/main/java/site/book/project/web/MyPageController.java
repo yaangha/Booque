eaa5eb680d8cbace30fe221f62713db7f3aa017a
choisilver <zmyn1004@gmail.com>
2022-12-19 17:40:17 +0900

    카트에서 금액 바뀜!

--- src/main/java/site/book/project/web/MyPageController.java
+++ src/main/java/site/book/project/web/MyPageController.java
@@ -2,21 +2,29 @@ package site.book.project.web;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import site.book.project.domain.User;
 import site.book.project.dto.UserSecurityDto;
+import site.book.project.repository.UserRepository;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class MyPageController {
     
+    private final UserRepository userRepository;
+    
+    
     // (하은) 마이페이지 연결
     @GetMapping("/myPage")
-    public String myPage(@AuthenticationPrincipal UserSecurityDto userSecurityDto) {
+    public String myPage(@AuthenticationPrincipal UserSecurityDto u, Model model) {
         
+        User user = userRepository.findById(u.getId()).get();
+        model.addAttribute("user", user);
         
         
         return "/book/myPage";
