413881c26f2151115840c4df45754778c4e79d96
Jihye-Ryu <limegrey876@gmail.com>
2022-12-20 11:02:09 +0900

    jihye_1220_1
    홈화면 컨텐츠,자바스크립트 효과

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -4,25 +4,21 @@
  
  // 홈(메인) 화면 컨텐츠에 쓰일 자바스크립트
  
-    // Category's BEST
-function openTap(e, category) {
+// 카테고리별 BEST
+function openTap(e, tapContent) {
+    const content = document.querySelectorAll('.tapContent');  // 모든 탭 내용 찾기
+    const button = document.querySelectorAll('.btnTap');  // 모든 탭 버튼 찾기  
+    
     let i;
-    let cateTap = document.querySelectorAll('cateTap');  // 모든 탭내용 찾기
-      
-    for (i = 0; i < cateTap.length; i++) {
-        cateTap[i].style.display = "none";    // 탭내용들 숨기기
+    for (i = 0; i < content.length; i++) {
+        content[i].style.display = "none";    // 탭 내용들 숨기기
+        button[i].className = button[i].className.replace(" w3-black", "");  // 탭 버튼들 강조효과 지우기
     }
-    
-    let button = document.querySelectorAll('btnTap');  // 탭버튼들 찾기
-    
-    for (i = 0; i<cateTap.length; i++) {
-        button[i].className = button[i].className.replace(" w3-dark-grey", "");
-    }  // 모든 탭버튼들 강조효과 지우기
       
-      document.getElementById(category).style.display = "block";
-      e.currentTarget.className += " w3-dark-grey";
+      document.getElementById(tapContent).style.display = "block";
+      e.currentTarget.className += " w3-black";
     }
 
-    let btnNow = document.querySelectorAll('btnTap')[0];
-    btnNow.click();
+    const btnDefault = document.querySelectorAll('.btnTap')[0];  // 첫번째 탭 열어 놓기
+    btnDefault.click();
     
