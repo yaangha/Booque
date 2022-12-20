be8e6bfbf7732c5baca69449317cb5bcd659f088
epepssp <101303ee@naver.com>
2022-12-20 10:22:55 +0900

    예진 언니 1220 작업본!

--- src/main/java/site/book/project/web/MyPageController.java
+++ src/main/java/site/book/project/web/MyPageController.java
@@ -1,10 +1,12 @@
 package site.book.project.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import site.book.project.dto.UserSecurityDto;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -13,7 +15,7 @@ public class MyPageController {
     
     // (하은) 마이페이지 연결
     @GetMapping("/myPage")
-    public String myPage(Integer userId) {
+    public String myPage(@AuthenticationPrincipal UserSecurityDto userSecurityDto) {
         
         
         return "/book/myPage";
